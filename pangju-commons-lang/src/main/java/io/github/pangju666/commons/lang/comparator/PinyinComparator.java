ca74b9015b02823eecbf86b37659c64ce302f6fb
majiale <2281525307@qq.com>
2025-03-24 09:26:56 +0800

    perf(lang): 优化PinyinComparator，新增order静态方法并完善测试

--- pangju-commons-lang/src/main/java/io/github/pangju666/commons/lang/comparator/PinyinComparator.java
+++ pangju-commons-lang/src/main/java/io/github/pangju666/commons/lang/comparator/PinyinComparator.java
@@ -3,7 +3,9 @@ package io.github.pangju666.commons.lang.comparator;
 import com.hankcs.hanlp.HanLP;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
 
 public final class PinyinComparator implements Comparator<String> {
@@ -17,19 +19,47 @@ public final class PinyinComparator implements Comparator<String> {
 		this.separator = separator;
 	}
 
+	public static void order(List<String> list) {
+		list.sort(new PinyinComparator());
+	}
+
+	public static void order(List<String> list, String separator) {
+		list.sort(new PinyinComparator(separator));
+	}
+
+	public static void order(String[] array) {
+		Arrays.sort(array, new PinyinComparator());
+	}
+
+	public static void order(String[] array, String separator) {
+		Arrays.sort(array, new PinyinComparator(separator));
+	}
+
 	@Override
 	public int compare(String o1, String o2) {
 		if (StringUtils.compare(o1, o2) == 0) {
 			return 0;
 		}
 		if (Objects.isNull(o1)) {
-			return 1;
-		}
-		if (Objects.isNull(o2)) {
 			return -1;
 		}
-		String o1PinYin = HanLP.convertToPinyinString(o1, separator, true);
-		String o2PinYin = HanLP.convertToPinyinString(o1, separator, true);
+		if (Objects.isNull(o2)) {
+			return 1;
+		}
+		if (o1.isEmpty()) {
+			return -1;
+		}
+		if (o2.isEmpty()) {
+			return 1;
+		}
+		if (o1.isBlank()) {
+			return -1;
+		}
+		if (o2.isBlank()) {
+			return 1;
+		}
+		String o1PinYin = HanLP.convertToPinyinString(o1, separator, false);
+		String o2PinYin = HanLP.convertToPinyinString(o2, separator, false);
 		return o1PinYin.compareTo(o2PinYin);
 	}
 }
--- pangju-commons-lang/src/test/groovy/io/github/pangju666/commons/lang/comparator/PinyinComparatorTest.groovy
+++ pangju-commons-lang/src/test/groovy/io/github/pangju666/commons/lang/comparator/PinyinComparatorTest.groovy
@@ -1,8 +1,21 @@
 package io.github.pangju666.commons.lang.comparator
 
+
 import spock.lang.Specification
 
 class PinyinComparatorTest extends Specification {
 	def "Compare"() {
+		given:
+		def comparator = new PinyinComparator()
+
+		expect:
+		list.toSorted(comparator) == result
+
+		where:
+		list                                               || result
+		[null, "", "天气如何"]                             || [null, "", "天气如何"]
+		["天气如何", null, ""]                             || [null, "", "天气如何"]
+		["天气如何", "测试字符串", "Best Java"]            || ["Best Java", "测试字符串", "天气如何"]
+		["天气如何???", "!!!!!测试字符串", "Best Java..."] || ["!!!!!测试字符串", "Best Java...", "天气如何???"]
 	}
 }
