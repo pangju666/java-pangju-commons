83b379470d7b7b30486612b4e7aefac2dd3cdf3b
胖橘 <nullptr255@outlook.com>
2025-03-28 00:55:05 +0800

    style(image): 格式调整

--- pangju-commons-image/src/main/java/io/github/pangju666/commons/image/lang/ImageConstants.java
+++ pangju-commons-image/src/main/java/io/github/pangju666/commons/image/lang/ImageConstants.java
@@ -5,13 +5,14 @@ import java.util.Objects;
 import java.util.Set;
 
 public class ImageConstants {
-	public static final String JPEG_MIME_TYPE = "image/jpeg";
-
 	public static int WHITE_HEX_RGB = 0xFFFFFFFF;
 	public static int BLACK_HEX_RGB = 0xFF000000;
+
+	public static final String JPEG_MIME_TYPE = "image/jpeg";
 	public static final String PNG_MIME_TYPE = "image/png";
 	public static final String WEBP_MIME_TYPE = "image/webp";
 	public static final String BMP_MIME_TYPE = "image/bmp";
+
 	private static volatile Set<String> SUPPORT_IMAGE_TYPES;
 
 	public static Set<String> getSupportImageTypes() {
