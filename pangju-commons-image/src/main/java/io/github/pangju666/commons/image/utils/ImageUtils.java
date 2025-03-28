b84ed8fd7df3a16be248416364b17f47006dafe6
majiale <2281525307@qq.com>
2025-03-28 17:48:48 +0800

    perf(image):  1.优化ImageUtils  2.增加测试图片，完善测试

--- pangju-commons-image/src/main/java/io/github/pangju666/commons/image/utils/ImageUtils.java
+++ pangju-commons-image/src/main/java/io/github/pangju666/commons/image/utils/ImageUtils.java
@@ -19,11 +19,14 @@ package io.github.pangju666.commons.image.utils;
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataReader;
 import com.drew.metadata.exif.ExifDirectoryBase;
+import com.drew.metadata.exif.ExifIFD0Directory;
 import com.drew.metadata.file.FileTypeDirectory;
 import io.github.pangju666.commons.image.model.ImageSize;
 import io.github.pangju666.commons.io.utils.FileUtils;
 import io.github.pangju666.commons.io.utils.IOUtils;
+import org.apache.commons.io.input.UnsynchronizedByteArrayInputStream;
 import org.apache.commons.io.output.UnsynchronizedByteArrayOutputStream;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.ObjectUtils;
@@ -34,6 +37,7 @@ import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
 import javax.imageio.spi.ImageReaderSpi;
 import javax.imageio.stream.ImageInputStream;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -123,7 +127,9 @@ public class ImageUtils {
 		if (ArrayUtils.isEmpty(bytes)) {
 			return false;
 		}
-		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(bytes)) {
+
+		InputStream inputStream = IOUtils.toUnsynchronizedByteArrayInputStream(bytes);
+		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream)) {
 			return Objects.nonNull(getImageType(imageInputStream));
 		}
 	}
@@ -140,8 +146,15 @@ public class ImageUtils {
 	 */
 	public static boolean isSupportImageType(final InputStream inputStream) throws IOException {
 		Validate.notNull(inputStream, "inputStream 不可为 null");
-		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream)) {
-			return Objects.nonNull(getImageType(imageInputStream));
+		if (inputStream instanceof BufferedInputStream bufferedInputStream) {
+			try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(bufferedInputStream)) {
+				return Objects.nonNull(getImageType(imageInputStream));
+			}
+		} else {
+			try (BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+				 ImageInputStream imageInputStream = ImageIO.createImageInputStream(bufferedInputStream)) {
+				return Objects.nonNull(getImageType(imageInputStream));
+			}
 		}
 	}
 
@@ -169,8 +182,9 @@ public class ImageUtils {
 			reader.dispose();
 			return false;
 		}
+		String[] mimeTypes = readerSpi.getMIMETypes();
 		reader.dispose();
-		return ArrayUtils.contains(readerSpi.getMIMETypes(), mimeType2);
+		return ArrayUtils.contains(mimeTypes, mimeType2);
 	}
 
 	/**
@@ -227,7 +241,9 @@ public class ImageUtils {
 			return false;
 		}
 		Validate.notBlank(mimeType, "mimeType 不可为空");
-		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(bytes)) {
+
+		InputStream inputStream = IOUtils.toUnsynchronizedByteArrayInputStream(bytes);
+		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream)) {
 			return ArrayUtils.contains(parseImageTypes(imageInputStream), mimeType);
 		}
 	}
@@ -246,8 +262,15 @@ public class ImageUtils {
 	public static boolean isSameImageType(final InputStream inputStream, final String mimeType) throws IOException {
 		Validate.notNull(inputStream, "inputStream 不可为 null");
 		Validate.notBlank(mimeType, "mimeType 不可为空");
-		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream)) {
-			return ArrayUtils.contains(parseImageTypes(imageInputStream), mimeType);
+		if (inputStream instanceof BufferedInputStream bufferedInputStream) {
+			try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(bufferedInputStream)) {
+				return ArrayUtils.contains(parseImageTypes(imageInputStream), mimeType);
+			}
+		} else {
+			try (BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+				 ImageInputStream imageInputStream = ImageIO.createImageInputStream(bufferedInputStream)) {
+				return ArrayUtils.contains(parseImageTypes(imageInputStream), mimeType);
+			}
 		}
 	}
 
@@ -278,19 +301,21 @@ public class ImageUtils {
 	 * @return 检测到的MIME类型，未识别返回null
 	 * @throws IOException 文件读取失败时抛出
 	 * @see ImageReaderSpi
-	 * @see com.drew.metadata.MetadataReader
+	 * @see MetadataReader
 	 * @since 1.0.0
 	 */
 	public static String getImageType(final File file) throws IOException {
 		FileUtils.checkExists(file, "file 不可为 null", true);
+
 		try {
 			Metadata metadata = ImageMetadataReader.readMetadata(file);
 			String mimeType = getImageType(metadata);
 			if (StringUtils.isNotBlank(mimeType)) {
 				return mimeType;
 			}
-		} catch (ImageProcessingException ignored) {
+		} catch (ImageProcessingException | IOException ignored) {
 		}
+
 		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(file)) {
 			return getImageType(imageInputStream);
 		}
@@ -308,19 +333,21 @@ public class ImageUtils {
 	 * @return 检测到的MIME类型，未识别返回null
 	 * @throws IOException 文件读取失败时抛出
 	 * @see ImageReaderSpi
-	 * @see com.drew.metadata.MetadataReader
+	 * @see MetadataReader
 	 * @since 1.0.0
 	 */
 	public static String getImageType(final Path path) throws IOException {
 		checkPath(path, "path 不可为 null");
+
 		try {
 			Metadata metadata = ImageMetadataReader.readMetadata(path.toFile());
 			String mimeType = getImageType(metadata);
 			if (StringUtils.isNotBlank(mimeType)) {
 				return mimeType;
 			}
-		} catch (ImageProcessingException ignored) {
+		} catch (ImageProcessingException | IOException ignored) {
 		}
+
 		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(path.toFile())) {
 			return getImageType(imageInputStream);
 		}
@@ -338,22 +365,26 @@ public class ImageUtils {
 	 * @return 检测到的MIME类型，未识别返回null
 	 * @throws IOException 数据解析失败时抛出
 	 * @see ImageReaderSpi
-	 * @see com.drew.metadata.MetadataReader
+	 * @see MetadataReader
 	 * @since 1.0.0
 	 */
 	public static String getImageType(final byte[] bytes) throws IOException {
 		if (ArrayUtils.isEmpty(bytes)) {
 			return null;
 		}
+		UnsynchronizedByteArrayInputStream inputStream = IOUtils.toUnsynchronizedByteArrayInputStream(bytes);
+
 		try {
-			Metadata metadata = ImageMetadataReader.readMetadata(IOUtils.toUnsynchronizedByteArrayInputStream(bytes));
+			Metadata metadata = ImageMetadataReader.readMetadata(inputStream);
 			String mimeType = getImageType(metadata);
 			if (StringUtils.isNotBlank(mimeType)) {
 				return mimeType;
 			}
-		} catch (ImageProcessingException ignored) {
+			inputStream.reset();
+		} catch (ImageProcessingException | IOException ignored) {
 		}
-		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(bytes)) {
+
+		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream)) {
 			return getImageType(imageInputStream);
 		}
 	}
@@ -370,21 +401,21 @@ public class ImageUtils {
 	 * @return 检测到的MIME类型，未识别返回null
 	 * @throws IOException 流读取失败时抛出
 	 * @see ImageReaderSpi
-	 * @see com.drew.metadata.MetadataReader
+	 * @see MetadataReader
 	 * @since 1.0.0
 	 */
 	public static String getImageType(final InputStream inputStream) throws IOException {
 		Validate.notNull(inputStream, "inputStream 不可为 null");
-		try {
-			Metadata metadata = ImageMetadataReader.readMetadata(inputStream);
-			String mimeType = getImageType(metadata);
-			if (StringUtils.isNotBlank(mimeType)) {
-				return mimeType;
+
+		if (inputStream instanceof BufferedInputStream bufferedInputStream) {
+			try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(bufferedInputStream)) {
+				return getImageType(imageInputStream);
 			}
-		} catch (ImageProcessingException ignored) {
-		}
-		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream)) {
-			return getImageType(imageInputStream);
+		} else {
+			try (BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+				 ImageInputStream imageInputStream = ImageIO.createImageInputStream(bufferedInputStream)) {
+				return getImageType(imageInputStream);
+			}
 		}
 	}
 
@@ -430,7 +461,7 @@ public class ImageUtils {
 	 * @return 图像尺寸，未识别返回null
 	 * @throws IOException 文件读取失败时抛出
 	 * @see ImageReader
-	 * @see com.drew.metadata.MetadataReader
+	 * @see MetadataReader
 	 * @since 1.0.0
 	 */
 	public static ImageSize getImageSize(final File file) throws IOException {
@@ -442,7 +473,7 @@ public class ImageUtils {
 			if (Objects.nonNull(imageSize)) {
 				return imageSize;
 			}
-		} catch (ImageProcessingException ignored) {
+		} catch (ImageProcessingException | IOException ignored) {
 		}
 
 		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(file)) {
@@ -463,14 +494,29 @@ public class ImageUtils {
 	 *
 	 * @param path 文件路径
 	 * @return 图像尺寸，未识别返回null
-	 * @throws IOException              文件读取失败时抛出
+	 * @throws IOException 文件读取失败时抛出
 	 * @see ImageReader
-	 * @see com.drew.metadata.MetadataReader
+	 * @see MetadataReader
 	 * @since 1.0.0
 	 */
 	public static ImageSize getImageSize(final Path path) throws IOException {
 		checkPath(path, "path 不可为 null");
-		return getImageSize(path.toFile());
+
+		try {
+			Metadata metadata = ImageMetadataReader.readMetadata(path.toFile());
+			ImageSize imageSize = getImageSize(metadata);
+			if (Objects.nonNull(imageSize)) {
+				return imageSize;
+			}
+		} catch (ImageProcessingException | IOException ignored) {
+		}
+
+		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(path.toFile())) {
+			if (Objects.isNull(imageInputStream)) {
+				return null;
+			}
+			return getImageSize(imageInputStream);
+		}
 	}
 
 	/**
@@ -483,16 +529,33 @@ public class ImageUtils {
 	 *
 	 * @param bytes 图像数据
 	 * @return 图像尺寸，未识别返回null
-	 * @throws IOException              数据解析失败时抛出
+	 * @throws IOException 数据解析失败时抛出
 	 * @see ImageReader
-	 * @see com.drew.metadata.MetadataReader
+	 * @see MetadataReader
 	 * @since 1.0.0
 	 */
 	public static ImageSize getImageSize(final byte[] bytes) throws IOException {
 		if (ArrayUtils.isEmpty(bytes)) {
 			return null;
 		}
-		return parseImageSize(IOUtils.toUnsynchronizedByteArrayOutputStream(bytes));
+		UnsynchronizedByteArrayInputStream inputStream = IOUtils.toUnsynchronizedByteArrayInputStream(bytes);
+
+		try {
+			Metadata metadata = ImageMetadataReader.readMetadata(inputStream);
+			ImageSize imageSize = getImageSize(metadata);
+			if (Objects.nonNull(imageSize)) {
+				return imageSize;
+			}
+			inputStream.reset();
+		} catch (ImageProcessingException | IOException ignored) {
+		}
+
+		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream)) {
+			if (Objects.isNull(imageInputStream)) {
+				return null;
+			}
+			return getImageSize(imageInputStream);
+		}
 	}
 
 	/**
@@ -507,12 +570,28 @@ public class ImageUtils {
 	 * @return 图像尺寸，未识别返回null
 	 * @throws IOException 流读取失败时抛出
 	 * @see ImageReader
-	 * @see com.drew.metadata.MetadataReader
+	 * @see MetadataReader
 	 * @since 1.0.0
 	 */
-	public static ImageSize getImageSize(final InputStream inputStream) throws IOException {
+	public static ImageSize getImageSize(InputStream inputStream) throws IOException {
 		Validate.notNull(inputStream, "inputStream 不可为 null");
-		return parseImageSize(IOUtils.toUnsynchronizedByteArrayOutputStream(inputStream));
+		UnsynchronizedByteArrayOutputStream outputStream = IOUtils.toUnsynchronizedByteArrayOutputStream(inputStream);
+		//TODO 解决大图片解析慢问题
+		try {
+			Metadata metadata = ImageMetadataReader.readMetadata(outputStream.toInputStream());
+			ImageSize imageSize = getImageSize(metadata);
+			if (Objects.nonNull(imageSize)) {
+				return imageSize;
+			}
+		} catch (ImageProcessingException | IOException ignored) {
+		}
+
+		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(outputStream.toInputStream())) {
+			if (Objects.isNull(imageInputStream)) {
+				return null;
+			}
+			return getImageSize(imageInputStream);
+		}
 	}
 
 	/**
@@ -552,16 +631,18 @@ public class ImageUtils {
 		Integer imageHeight = null;
 		int imageOrientation = NORMAL_ORIENTATION;
 		for (ExifDirectoryBase exifDirectory : exifDirectories) {
-			if (exifDirectory.containsTag(ExifDirectoryBase.TAG_EXIF_IMAGE_WIDTH)) {
-				Integer width = exifDirectory.getInteger(ExifDirectoryBase.TAG_EXIF_IMAGE_WIDTH);
-				if (Objects.nonNull(width)) {
-					imageWidth = width;
+			if (exifDirectory instanceof ExifIFD0Directory exifIFD0Directory) {
+				if (exifIFD0Directory.containsTag(ExifDirectoryBase.TAG_IMAGE_WIDTH)) {
+					Integer width = exifIFD0Directory.getInteger(ExifDirectoryBase.TAG_IMAGE_WIDTH);
+					if (Objects.nonNull(width)) {
+						imageWidth = width;
+					}
 				}
-			}
-			if (exifDirectory.containsTag(ExifDirectoryBase.TAG_EXIF_IMAGE_HEIGHT)) {
-				Integer height = exifDirectory.getInteger(ExifDirectoryBase.TAG_EXIF_IMAGE_HEIGHT);
-				if (Objects.nonNull(height)) {
-					imageHeight = height;
+				if (exifIFD0Directory.containsTag(ExifDirectoryBase.TAG_IMAGE_HEIGHT)) {
+					Integer height = exifIFD0Directory.getInteger(ExifDirectoryBase.TAG_IMAGE_HEIGHT);
+					if (Objects.nonNull(height)) {
+						imageHeight = height;
+					}
 				}
 			}
 			Integer orientation = exifDirectory.getInteger(ExifDirectoryBase.TAG_ORIENTATION);
@@ -585,48 +666,17 @@ public class ImageUtils {
 	 * @since 1.0.0
 	 */
 	protected static String[] parseImageTypes(final ImageInputStream imageInputStream) {
+		String[] mimeTypes = ArrayUtils.EMPTY_STRING_ARRAY;
 		Iterator<ImageReader> readers = ImageIO.getImageReaders(imageInputStream);
 		if (readers.hasNext()) {
 			ImageReader reader = readers.next();
 			ImageReaderSpi readerSpi = reader.getOriginatingProvider();
-			reader.dispose();
 			if (Objects.nonNull(readerSpi)) {
-				return ArrayUtils.nullToEmpty(readerSpi.getMIMETypes());
+				mimeTypes = ArrayUtils.nullToEmpty(readerSpi.getMIMETypes());
 			}
+			reader.dispose();
 		}
-		return ArrayUtils.EMPTY_STRING_ARRAY;
-	}
-
-	/**
-	 * 解析图像尺寸核心方法（双检测机制）
-	 * <p>处理流程：</p>
-	 * <ol>
-	 *   <li>优先通过元数据解析尺寸（支持方向校正）</li>
-	 *   <li>失败后使用ImageIO获取原始尺寸</li>
-	 *   <li>自动管理图像输入流资源</li>
-	 * </ol>
-	 *
-	 * @param outputStream 包含图像数据的字节输出流
-	 * @return 图像尺寸，解析失败返回null
-	 * @throws IOException 当流操作失败时抛出
-	 * @since 1.0.0
-	 */
-	protected static ImageSize parseImageSize(UnsynchronizedByteArrayOutputStream outputStream) throws IOException {
-		try {
-			Metadata metadata = ImageMetadataReader.readMetadata(outputStream.toInputStream());
-			ImageSize imageSize = getImageSize(metadata);
-			if (Objects.nonNull(imageSize)) {
-				return imageSize;
-			}
-		} catch (ImageProcessingException ignored) {
-		}
-
-		try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(outputStream.toInputStream())) {
-			if (Objects.isNull(imageInputStream)) {
-				return null;
-			}
-			return getImageSize(imageInputStream);
-		}
+		return mimeTypes;
 	}
 
 	/**
---
+++ pangju-commons-image/src/test/groovy/io/github/pangju666/commons/image/utils/ImageUtilsSpec.groovy
@@ -0,0 +1,185 @@
+package io.github.pangju666.commons.image.utils
+
+import spock.lang.Specification
+import spock.lang.TempDir
+import spock.lang.Unroll
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class ImageUtilsSpec extends Specification {
+	@TempDir
+	Path tempDir
+
+	// 测试资源路径
+	static final String TEST_IMAGES_DIR = "src/test/resources/"
+
+	static final String[] TEST_IMAGES = [
+		"${TEST_IMAGES_DIR}test.bmp",
+		"${TEST_IMAGES_DIR}test.gif",
+		"${TEST_IMAGES_DIR}test.ico",
+		"${TEST_IMAGES_DIR}test.iff",
+		"${TEST_IMAGES_DIR}test.jpg",
+		"${TEST_IMAGES_DIR}test.pcx",
+		"${TEST_IMAGES_DIR}test.png",
+		"${TEST_IMAGES_DIR}test.svg",
+		"${TEST_IMAGES_DIR}test.tga",
+		"${TEST_IMAGES_DIR}test.tif",
+		"${TEST_IMAGES_DIR}test.webp",
+		"${TEST_IMAGES_DIR}test.xwd"
+	]
+
+	def "test"() {
+		setup:
+		/*for (final def mimeType in Set.of(ImageIO.getReaderMIMETypes())) {
+			println mimeType
+		}*/
+		println ImageUtils.getImageSize(new FileInputStream(new File("D:\\workspace\\resource\\图片\\wac_nearside.tif")))
+	}
+
+	// 测试 isSupportImageType 系列方法
+	@Unroll
+	def "isSupportImageType应该正确识别文件"() {
+		given:
+		def file = new File(TEST_IMAGES[index])
+
+		when:
+		def result = ImageUtils.isSupportImageType(inputType == 'File' ? file :
+			inputType == 'Path' ? file.toPath() :
+				inputType == 'Bytes' ? Files.readAllBytes(file.toPath()) :
+					new FileInputStream(file))
+
+		then:
+		result == expected
+
+		where:
+		index | inputType     || expected
+		0     | 'File'        || true
+		1     | 'Path'        || true
+		2     | 'Bytes'       || true
+		3     | 'InputStream' || true
+		4     | 'File'        || true
+		5     | 'File'        || true
+	}
+
+	// 测试 isSameImageType 系列方法
+	@Unroll
+	def "isSameImageType应该正确比较"() {
+		given:
+		def testData = TEST_IMAGES[index]
+		def input = inputType == 'File' ? new File(testData) :
+			inputType == 'Path' ? Paths.get(testData) :
+				inputType == 'Bytes' ? Files.readAllBytes(Paths.get(testData)) :
+					new FileInputStream(testData)
+
+		when:
+		def result = ImageUtils.isSameImageType(input, mimeType)
+
+		then:
+		result == expected
+
+		where:
+		index | inputType     | mimeType                   || expected
+		0     | 'File'        | 'image/x-bmp'              || true
+		10    | 'Path'        | 'image/x-webp'             || true
+		2     | 'Bytes'       | 'image/vnd.microsoft.icon' || true
+		9     | 'InputStream' | 'image/x-tiff'             || true
+		6     | 'File'        | 'image/jpeg'               || false
+		5     | 'File'        | 'image/x-pcx'              || true
+		3     | 'File'        | 'image/x-iff'              || true
+		8     | 'File'        | 'image/x-tga'              || true
+	}
+
+	// 测试 getImageType 系列方法
+	@Unroll
+	def "getImageType应该正确识别的类型"() {
+		given:
+		def testData = TEST_IMAGES[index]
+		def input = inputType == 'File' ? new File(testData) :
+			inputType == 'Path' ? Paths.get(testData) :
+				inputType == 'Bytes' ? Files.readAllBytes(Paths.get(testData)) :
+					new FileInputStream(testData)
+
+		when:
+		def result = ImageUtils.getImageType(input)
+
+		then:
+		result == expectedType
+
+		where:
+		index | inputType     | expectedType
+		0     | 'File'        | 'image/bmp'
+		10    | 'Path'        | 'image/webp'
+		2     | 'Bytes'       | 'image/x-icon'
+		9     | 'InputStream' | 'image/tiff'
+		6     | 'File'        | 'image/png'
+		5     | 'File'        | 'image/x-pcx'
+		3     | 'File'        | 'image/iff'
+		8     | 'File'        | 'image/tga'
+	}
+
+	// 测试 getImageSize 系列方法
+	@Unroll
+	def "getImageSize应该正确获取的尺寸"() {
+		given:
+		def testData = TEST_IMAGES[index]
+		def input = inputType == 'File' ? new File(testData) :
+			inputType == 'Path' ? Paths.get(testData) :
+				inputType == 'Bytes' ? Files.readAllBytes(Paths.get(testData)) :
+					new FileInputStream(testData)
+
+		when:
+		def size = ImageUtils.getImageSize(input)
+
+		then:
+		size?.width() == expectedWidth
+		size?.height() == expectedHeight
+
+		where:
+		index | inputType     | expectedType   | expectedWidth | expectedHeight
+		0     | 'File'        | 'image/bmp'    | 600           | 600
+		10    | 'Path'        | 'image/webp'   | 1170          | 1428
+		2     | 'Bytes'       | 'image/x-icon' | 32            | 32
+		9     | 'InputStream' | 'image/tiff'   | 600           | 600
+		6     | 'File'        | 'image/png'    | 860           | 540
+		5     | 'File'        | 'image/x-pcx'  | 260           | 450
+		3     | 'File'        | 'image/iff'    | 320           | 200
+		8     | 'File'        | 'image/tga'    | 600           | 600
+	}
+
+	// 测试异常情况
+	def "getImageSize应该正确处理无效输入"() {
+		when:
+		def size = ImageUtils.getImageSize(new byte[0])
+
+		then:
+		size == null
+	}
+
+	// 添加性能测试
+	def "性能测试：处理大文件"() {
+		given:
+		def largeFile = new File("${TEST_IMAGES_DIR}/large.png")
+
+		when:
+		def result = ImageUtils.getImageSize(largeFile)
+
+		then:
+		result != null
+	}
+
+	// 添加并发测试
+	def "并发安全测试"() {
+		given:
+		def file = new File("${TEST_IMAGES_DIR}/large.png")
+
+		when:
+		def results = (1..100).parallelStream().map {
+			ImageUtils.getImageSize(file)
+		}.toList()
+
+		then:
+		results.every { it?.width() == 4095 }
+	}
+}
