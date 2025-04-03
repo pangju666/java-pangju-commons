b7abcdb33a98cd13ce440753b80a758a15bcd60c
majiale <2281525307@qq.com>
2025-04-03 13:57:19 +0800

    test(lang): 优化测试

--- pangju-commons-lang/src/test/groovy/io/github/pangju666/commons/lang/gson/GsonDeserializerSpec.groovy
+++ pangju-commons-lang/src/test/groovy/io/github/pangju666/commons/lang/gson/GsonDeserializerSpec.groovy
@@ -9,7 +9,15 @@ class GsonDeserializerSpec extends Specification {
 	def "test deserialize"() {
 		setup:
 		def object = JsonUtils.fromString(
-			"{\"value\":\"TEST\",\"date\":1743649073482,\"localDate\":1743649073482,\"localDateTime\":1743649073482}",
+			"{\n" +
+				"  \"phoneNumber\": \"133******26\",\n" +
+				"  \"value\": \"TEST\",\n" +
+				"  \"date\": 1743650529937,\n" +
+				"  \"localDate\": 1743609600000,\n" +
+				"  \"localDateTime\": 1743650529948,\n" +
+				"  \"decimal\": 10002.00,\n" +
+				"  \"aClass\": \"io.github.pangju666.commons.lang.model.TestData\"\n" +
+				"}",
 			TestData.class)
 		println object
 	}
@@ -25,10 +33,13 @@ class GsonDeserializerSpec extends Specification {
 	def "test deserialize error"() {
 		setup:
 		def object = JsonUtils.fromString("{\n" +
-			"  \"value\": 123,\n" +
-			"  \"date\": \"asdsad\",\n" +
-			"  \"localDate\": \"asdsad\",\n" +
-			"  \"localDateTime\": \"asdsad\"\n" +
+			"  \"phoneNumber\": \"133******26\",\n" +
+			"  \"value\": \"TEST\",\n" +
+			"  \"date\": 1743650529937,\n" +
+			"  \"localDate\": 1743609600000,\n" +
+			"  \"localDateTime\": 1743650529948,\n" +
+			"  \"decimal\": \"1000aaa2.0aa0\",\n" +
+			"  \"aClass\": \"ioa.github.pangju666.framework.core.model.TestData\"\n" +
 			"}", TestData.class)
 		println object
 	}
