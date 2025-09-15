c1e110e19276601b23bcab49001be1a4904865e7
胖橘 <nullptr255@outlook.com>
2025-09-15 08:20:38 +0800

    feat(lang): 正则表达式池增加符号字符正则

--- pangju-commons-lang/src/main/java/io/github/pangju666/commons/lang/pool/RegExPool.java
+++ pangju-commons-lang/src/main/java/io/github/pangju666/commons/lang/pool/RegExPool.java
@@ -49,6 +49,18 @@ public class RegExPool {
 	 */
 	public static final String CHINESE_CHARACTER = "(?:[\u3400-\u4DB5\u4E00-\u9FEA\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\uD840-\uD868\uD86A-\uD86C\uD86F-\uD872\uD874-\uD879][\uDC00-\uDFFF]|\uD869[\uDC00-\uDED6\uDF00-\uDFFF]|\uD86D[\uDC00-\uDF34\uDF40-\uDFFF]|\uD86E[\uDC00-\uDC1D\uDC20-\uDFFF]|\uD873[\uDC00-\uDEA1\uDEB0-\uDFFF]|\uD87A[\uDC00-\uDFE0])";
 	/**
+	 * 字符
+	 *
+	 * @since 1.0.0
+	 */
+	public static final String SYMBOLS_CHARACTER = "[_.!+-=—,$%^，。？、~@#￥…&*《》<>「」{}【】()（）/”“\"～！·]";
+	/**
+	 * 多个字符
+	 *
+	 * @since 1.0.0
+	 */
+	public static final String SYMBOLS_CHARACTERS = SYMBOLS_CHARACTER + "+";
+	/**
 	 * 中文汉字
 	 *
 	 * @since 1.0.0
